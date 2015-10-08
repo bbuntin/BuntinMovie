c2874529129212e52961cd360220f6bc2f945f64
Brad Buntin <bradley4@gmail.com>
2015-10-08 00:45:28 -0400

    working on fragments

--- app/src/main/java/bradley4/gmail/com/popularmovies/MovieDetail.java
+++ app/src/main/java/bradley4/gmail/com/popularmovies/MovieDetail.java
@@ -12,6 +12,11 @@ public class MovieDetail extends ActionBarActivity implements MovieDetailFragmen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.detail_container, new MovieDetailFragment())
+                    .commit();
+        }
     }
 
     @Override
